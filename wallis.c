4e6263b36723bb65d83fdad3508dc581135c422d
dkmaurya333 <dkmaurya333@gmail.com>
2021-05-30 12:53:08 +0530

    only function

--- wallis.c
+++ wallis.c
@@ -3,30 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
-
-int main(void) {
-int n;
- float pi;
- scanf("%d",&n);
- return (wallis_pi(n));
- //printf("%f",pi);
- 
-  /*for (int i=0; i<5; i++) {
-   pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
-    }
-  }
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
- if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
-    }
-  }*/
-}
 float wallis_pi(int n)
 { float x=1;
 for(float i=1;i<=n;i++)
@@ -34,6 +10,15 @@ for(float i=1;i<=n;i++)
 
 }
 return 2*x;}
+
+/*
+int main(void) {
+int n;
+ float pi;
+ scanf("%d",&n);
+ return (wallis_pi(n));
+
+}*/
 
 
 
