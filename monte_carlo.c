7c2ce36fa1beab06088eb29a6c20501312662ad6
dkmaurya333 <dkmaurya333@gmail.com>
2021-06-01 00:41:02 +0530

    kggffkjn

--- monte_carlo.c
+++ monte_carlo.c
@@ -19,46 +19,22 @@ float x=frandom();
 float y=frandom();
 float d=((x*x)+(y*y));
 if (d>1)
-{k++;
-//printf("(%f , %f)   -- %f----%f---%f\n",x,y,k,4*((i-k)/i),d);}
+k++;
 }
 float f=(i-k)/i;
 return 4*f;
-}}
+}
 
 
 
 int main(void) {
-//  float pi0;
-//  float pi1;
    int a;
 scanf("%d",&a);
 float ans=mc_pi(a);
-//printf("%f",ans);
+
 return ans;
   
-  //pi0 = mc_pi(2500);
-  //pi1 = mc_pi(2500);
- // printf("%f %f\n", pi0, pi1);
-  /*
-  if (pi0 == pi1) {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
-      abort();
-    }
-
-  if (fabs(pi0 - pi1) > 0.05) {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
-  }
-
-    
-  for (int i=2000; i<5000; i++) {
-    pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
-      abort();
-    }
-  }*/
+ 
 }
 
 
